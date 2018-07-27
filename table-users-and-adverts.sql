b0fc7285248830d644f670b5d20802e6e554a109
Bartlomiej <chwieja@gmail.com>
2018-07-27 18:06:54 +0200

    table-users-and-adverts

---
+++ table-users-and-adverts.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Users (
+    PersonID int,
+    Firstname varchar(255),
+    Lastname varchar(255),
+    BirthDate varchar(255),
+    City varchar(255),
+	Email varchar(255),
+	PersonDescript varchar(255)
+);
+
+CREATE TABLE Adverts (
+	AdvertID int,
+	Brand varchar(255),
+	Model varchar(255),
+	Engine varchar(255),
+	DateOfProduction varchar(255),
+	Milage varchar(255),
+	DateOfRegistrationInPoland varchar(255),
+	AdvertName varchar(255),
+	AdvertsDescript varchar(255)
+)
