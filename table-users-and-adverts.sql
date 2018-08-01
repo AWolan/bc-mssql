1e7af120c27fd53abc703879e8699aa9517c72b2
AWolan <amw.wolan@gmail.com>
2018-08-01 23:15:18 +0200

    Dodane komentarze

--- table-users-and-adverts.sql
+++ table-users-and-adverts.sql
@@ -1,21 +1,21 @@
 CREATE TABLE Users (
-    PersonID int,
+    PersonID int, -- lepiej zmienić nazwę na userId
     Firstname varchar(255),
     Lastname varchar(255),
-    BirthDate varchar(255),
+    BirthDate varchar(255), -- to powinna być data
     City varchar(255),
 	Email varchar(255),
 	PersonDescript varchar(255)
 );
 
-CREATE TABLE Adverts (
+CREATE TABLE Adverts ( -- brakuje połączenia z tabelą users
 	AdvertID int,
 	Brand varchar(255),
 	Model varchar(255),
 	Engine varchar(255),
-	DateOfProduction varchar(255),
-	Milage varchar(255),
-	DateOfRegistrationInPoland varchar(255),
+	DateOfProduction varchar(255), -- to powinna być data
+	Milage varchar(255), -- może lepiej żeby to była liczba
+	DateOfRegistrationInPoland varchar(255), -- to powinna być data
 	AdvertName varchar(255),
-	AdvertsDescript varchar(255)
+	AdvertsDescript varchar(255) -- ograniczenie opisu do 255 znaków może nie być dobrym pomysłem
 )
